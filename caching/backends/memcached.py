f63ae39332f211e1e1e100b72efe083d74cf9230
Joe Stump <joe@joestump.net>
2012-04-24 15:04:59 -0700

    Fixed naming up and corrected the base class for CacheClass.

--- caching/backends/memcached.py
+++ caching/backends/memcached.py
@@ -3,13 +3,13 @@ from django.utils.encoding import smart_str
 
 try:
     # Prefer the PyLibMCCache backend to the pure Python version.
-    from django.core.cache.backends.memcached import PyLibMCCache as CacheClass
+    from django.core.cache.backends.memcached import PyLibMCCache as BaseClass
 except ImportError:
-    from django.core.cache.backends.memcached import MemcachedCache as CacheClass
+    from django.core.cache.backends.memcached import MemcachedCache as BaseClass
 
 
 # Add infinite timeout support to the memcached backend.
-class CacheClass(memcached.CacheClass):
+class CacheClass(BaseClass):
 
     def add(self, key, value, timeout=None, version=None):
         if timeout is None:
