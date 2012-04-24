c1d84e10f4086417ae15c8d38ccad26284b53148
Joe Stump <joe@joestump.net>
2012-04-24 14:46:49 -0700

    Added conditional support for the PyLibMCCache backend.

--- caching/backends/memcached.py
+++ caching/backends/memcached.py
@@ -1,5 +1,11 @@
-from django.core.cache.backends import memcached
 from django.utils.encoding import smart_str
+
+
+try:
+    # Prefer the PyLibMCCache backend to the pure Python version.
+    from django.core.cache.backends.memcached import PyLibMCCache as CacheClass
+except ImportError:
+    from django.core.cache.backends.memcached import MemcachedCache as CacheClass
 
 
 # Add infinite timeout support to the memcached backend.
